6af7c099b2dbf54fcceb789247935367c35bc490
dodini <denis.michallet@free.fr>
2017-01-31 15:04:04 +0100

    Update Pacman.java
    J'ai essayé de faire des choses pour le fantôme en m'inspirant de ce que t'as fait pour le labyrinthe.

--- Pacman.java
+++ Pacman.java
@@ -6,11 +6,15 @@ public class Pacman extends JPanel {
 	int nombregum=248;
 	int pacmanX = 1;
 	int pacmanY = 1;
+	int ghostX = 18;
+	int ghostY = 29;
     public static final int NORD = 0b1;
     public static final int EST = 0b10;
     public static final int SUD = 0b100;
     public static final int OUEST = 0b1000;
-    int direction;
+    private int[] path = new int[2];
+    int directionPacman;
+    int GhostDirection;
     
 	int[] [] tableau = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 			{1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1},
@@ -44,10 +48,46 @@ public class Pacman extends JPanel {
 			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
     public Pacman() {
-    	
-    	
-    }
+    	}
+    public void ghostpath() {
+            path[0] = ghostX & ghostY;
+            int GhostDirection = EST;
+            while(ghostX != pacmanX && ghostY != pacmanY) {
+                if(tableau[ghostX][ghostY] / (GhostDirection*2%15) % 2 == 0) {
+                	GhostDirection = GhostDirection*2%15;
+                }
+                else if(tableau[ghostX][ghostY] / GhostDirection % 2 == 0) {
+                }
+                else if(tableau[ghostX][ghostY] / (GhostDirection*8%15) % 2 == 0) {
+                    GhostDirection = GhostDirection * 8 % 15;
+                }
+                else if(tableau[ghostX][ghostY] / (GhostDirection*4%15) % 2 == 0) {
+                    GhostDirection = GhostDirection * 4 % 15;
+                }
+                else {
+                    System.out.println("Probleme de direction");
+                }
+            }
+            repaint();
+            }
 
+    public void ghostmove(int GhostDirection) {
+        if((tableau[ghostX][ghostY] & GhostDirection) == 0) {
+            if(GhostDirection == NORD) {
+                ghostY = ghostY-1;
+            }
+            else if(GhostDirection == SUD) {
+            	ghostY = ghostY+1;
+            }
+            else if(GhostDirection == EST) {
+            	ghostX = ghostX+1;
+            }
+            else if(GhostDirection == OUEST) {
+            	ghostX = ghostX-1;
+            }
+            repaint();
+        }
+        }
     public void paintComponent(Graphics g) {
         // sert de background
         g.setColor(Color.WHITE);
@@ -62,6 +102,10 @@ public class Pacman extends JPanel {
     		  if(j == pacmanX && i == pacmanY) {
     			  g.setColor(Color.YELLOW);
         		  g.fillOval(getWidth()/20*j, getHeight()/31*i , getWidth()/20, getHeight()/31);
+    		  }
+    		  if(j == ghostX && i == ghostY){
+    			  g.setColor(Color.RED);
+    			  g.fillOval(getWidth()/20*j, getHeight()/31*i, getWidth()/20, getHeight()/31);
     		  }
     	  }
       }
